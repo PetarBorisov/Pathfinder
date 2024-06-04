163bd4d17d4d74ff372a65a0492766cb5afb5a82
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-06-04 23:01:29 +0300

    Update SecurityConfig.java

--- src/main/java/bg/softuni/pathfinder/config/SecurityConfig.java
+++ src/main/java/bg/softuni/pathfinder/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package bg.softuni.pathfinder.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,29 +13,48 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
         return httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        authorizeRequests ->
-                                authorizeRequests
-                                        .requestMatchers(
-                                                "/", "/users/login", "/users/register", "/about").permitAll()
-                                        .anyRequest().authenticated())
-                .formLogin(
-                        formLogin ->
-                                formLogin
-                                        .loginPage("/users/login")
-                                        .usernameParameter("username")
-                                        .passwordParameter("password")
-                                        .defaultSuccessUrl("/", true)
-                                        .failureForwardUrl("/about"))
-                .logout(
-                        logout ->
-                                logout
-                                        .logoutUrl("/users/logout")
-                                        .logoutSuccessUrl("/")
-                                        .invalidateHttpSession(true))
-                .build();
-    }
-}
+
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // Define which urls are visible by which users
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/comments/delete/{id}/**").hasRole("ADMIN")
+                        // All static resources which are situated in js, images, css are available for anyone
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        // Allow anyone to see the home page, the registration page and the login form
+                        .requestMatchers("/","/menu","/gallery","/users/login", "/users/register", "/users/login-error").permitAll()
+                        .requestMatchers("/api/comments/add","/api/comments/view").permitAll()
+                        // all other requests are authenticated.
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().authenticated()
+                ).formLogin(
+                        formLogin -> {
+                            formLogin
+                                    // redirect here when we access something which is not allowed.
+                                    // also this is the page where we perform login.
+                                    .loginPage("/users/login")
+                                    // The names of the input fields (in our case in auth-login.html)
+                                    .usernameParameter("username")
+                                    .passwordParameter("password")
+                                    .defaultSuccessUrl("/",true)
+                                    .failureForwardUrl("/users/login-error");
+                        }
+
+                ).logout(
+                        logout -> {
+                            logout
+                                    // the URL where we should POST something in order to perform the logout
+                                    .logoutUrl("/users/logout")
+                                    // where to go when logged out?
+                                    .logoutSuccessUrl("/")
+                                    // invalidate the HTTP session
+                                    .invalidateHttpSession(true);
+                        }
+
+
+                ).build();
+
+    }}
